9c660f199d6534b7e2321d0589c0713eacb40972
Billingbuddy <surjabakery@gmail.com>
2026-01-23 23:26:47 +0530

    update

--- clints/vite.config.js
+++ clints/vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  // Ye line Vercel ke liye bilkul zaroori hai
+  base: "/",
+})
